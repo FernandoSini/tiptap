67f4fa79f95479645dfa8740f5a686b25fe0796a
Fernando Fazio Sinigaglia <fernandosini@MacBook-Air-de-Fernando.local>
2021-10-13 21:40:51 -0300

    a

--- lib/main.dart
+++ lib/main.dart
@@ -17,34 +17,37 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TipTap',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(),
       initialRoute: "/home",
-      onGenerateRoute:kIsWeb? (settings) {
-        switch (settings.name) {
-          case "/home":
-            return PageRouteBuilder(
-              settings: settings,
-              pageBuilder: (context, animation1, animation2) =>
-                  const WebHomeScreen(),
-            );
-          // case "/aboutus":
-          //   return PageRouteBuilder(
-          //     settings: settings,
-          //     pageBuilder: (context, animation1, animation2) => AboutUsScreen(),
-          //   );
-          // case "/help":
-          //   return PageRouteBuilder(
-          //     settings: settings,
-          //     pageBuilder: (context, animation1, animation2) => HelpScreen(),
-          //   );
-          // case "/privacy":
-          //   return PageRouteBuilder(
-          //     settings: settings,
-          //     pageBuilder: (context, animation1, animation2) => PrivacyScreen(),
-          //   );
-          
-        }
-      }:null,
+      onGenerateRoute: kIsWeb
+          ? (settings) {
+              switch (settings.name) {
+                case "/home":
+                  return PageRouteBuilder(
+                    settings: settings,
+                    pageBuilder: (context, animation1, animation2) =>
+                        const WebHomeScreen(),
+                  );
+                // case "/aboutus":
+                //   return PageRouteBuilder(
+                //     settings: settings,
+                //     pageBuilder: (context, animation1, animation2) => AboutUsScreen(),
+                //   );
+                // case "/help":
+                //   return PageRouteBuilder(
+                //     settings: settings,
+                //     pageBuilder: (context, animation1, animation2) => HelpScreen(),
+                //   );
+                // case "/privacy":
+                //   return PageRouteBuilder(
+                //     settings: settings,
+                //     pageBuilder: (context, animation1, animation2) => PrivacyScreen(),
+                //   );
+
+              }
+            }
+          : null,
       // home: kIsWeb ? const HomeScreen() : null,
     );
   }
